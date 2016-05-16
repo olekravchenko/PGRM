ff29ca9a61f7ab49e0a434ff692b59a7b804be54
VVBondarenko <vbondarenko3950@yandex.ru>
2016-05-17 00:56:38 +0300

    Tried to detect bugs. Possibly: omega function multyplying/dividing lost somewhere.

--- Poisson/poisson2d_rfm.m
+++ Poisson/poisson2d_rfm.m
@@ -41,10 +41,10 @@ addpath('core')
 addpath('core_rfm')
 
 %% Initialization
-a = -5; b = -a;
-c = -1; d = -c;
+a = -pi; b = -a;
+c = -pi; d = -c;
 
-nx = 2^7;
+nx = 2^5;
 ny = nx;
 dx = (b-a)/(nx-1);
 dy = (d-c)/(ny-1);
@@ -54,8 +54,14 @@ ty = c:dy:d;
 [x, y] = meshgrid(tx, ty);
 
 %% Input functions
-rhs_func    = @(x,y) 2*x.*y.*exp(x+y).*(x+y+x.*y-3);    % Right hand side function
-ua          = @(x,y) x.*(x-1).*y.*(y-1).*exp(x+y);      % Exact Solution
+%rhs_func    = @(x,y) 2*x.*y.*exp(x+y).*(x+y+x.*y-3);    % Right hand side function
+%ua          = @(x,y) x.*(x-1).*y.*(y-1).*exp(x+y);      % Exact Solution
+
+rhs_func    = @(x,y) -2*sin(x).*sin(y);    % Right hand side function
+ua          = @(x,y) sin(x).*sin(y);      % Exact Solution
+
+
+
 
 %% Omega function
 % om1 = 0.5*(0.5 - (x - 0.5).^2 - (y - 0.5).^2 - ...
@@ -67,8 +73,10 @@ ua          = @(x,y) x.*(x-1).*y.*(y-1).*exp(x+y);      % Exact Solution
 % omega = (0.25 - (x - 0.5).^2) .* (0.25 - (y - 0.5).^2);
 % omega(omega<0) = 0;
 % omega = x.*(x-b).*y.*(y-d);
-om1 = x + b; om2 = y + d; om3 = b - x; om4 = d - y;
-om12 = r_con(om1, om2); om34 = r_con(om3, om4);
+om1 = x + b; om2 = y + d; 
+om3 = b - x; om4 = d - y;
+om12 = r_con(om1, om2); 
+om34 = r_con(om3, om4);
 omega = r_con(om12, om34);
 
 u_appr = 0*omega;
@@ -80,20 +88,20 @@ u_appr = 0*omega;
 % surf(x, y, om_ch)
 % shading interp
 % lighting phong
-% title('Characteristic function of $\omega(x,y)$','Interpreter','latex')
+% title('Characteristic function of \omega(x,y)','Interpreter','latex')
 % axis square
 % % view(0,90)
 % % axis off
-% xlabel('$x$','Interpreter','latex')
-% ylabel('$y$','Interpreter','latex')
-% zlabel('$\widetilde{\omega}(x,y)$','Interpreter','latex')
+% xlabel('x','Interpreter','latex')
+% ylabel('y','Interpreter','latex')
+% zlabel('\widetilde{\omega}(x,y)','Interpreter','latex')
 % 
 % u_appr = 0*omega;
 
 figure(1),
 surf(x, y, omega)
-shading interp; lighting phong; axis square; axis off; camlight left
-title('$\omega(x,y)$','Interpreter','latex')
+shading interp;  axis square; %axis off; 
+title('\omega(x,y)','Interpreter','tex')
 % axis equal
 
 figure(2),
@@ -104,15 +112,15 @@ for i = 1:nf
         subplot(nf,nf,k)
         tmp = basis_function(k, x, y);
         surf(x, y, omega.*tmp)
-        shading interp; lighting phong; axis square; axis off; camlight left
+        shading interp;  axis square; axis off; 
 %         view(36,33)
         view(0,90)
-        xlabel('$x$','Interpreter','latex')
-        ylabel('$y$','Interpreter','latex')
-        title(['$\psi_{' int2str(k) '}=' '\omega x^{' int2str(i-1) '}y^{' int2str(j-1) '}$'],'Interpreter','latex')
+        xlabel('x','Interpreter','tex')
+        ylabel('y','Interpreter','tex')
+        title(['\psi_{' int2str(k) '}=' '\omega x^{' int2str(i-1) '}y^{' int2str(j-1) '}'],'Interpreter','tex')
     end
 end
-%text(-5,-0.5,'Basis function $\psi_k(x,y)=x^iy^j$','Interpreter','latex')
+%text(-5,-0.5,'Basis function \psi_k(x,y)=x^iy^j','Interpreter','latex')
 
 %% Assembling
 K   = 16;
@@ -123,12 +131,12 @@ ind = 1:K;
 
 %% Computational Loop
 for l = ind
-    BF(l,:,:)   =   basis_function(l, x, y);
-    B(l)        = - trapz(tx, trapz(ty, rhs_func(x,y).*omega.*squeeze(BF(l,:,:))));    
-    for k = ind                
-        tmp     = del2(omega.*squeeze(BF(l,:,:)), dx, dy);
-        A(k, l) = trapz(tx, trapz(ty, omega.*squeeze(BF(k,:,:)).*tmp));
-    end % for l
+   BF(l,:,:)   = basis_function(l, x, y);
+   B(l)        = trapz(tx, trapz(ty, rhs_func(x,y).*omega.*squeeze(BF(l,:,:))));    
+   for k = ind                
+       tmp     = del2(omega.*squeeze(BF(l,:,:)), dx, dy);
+       A(k, l) = trapz(tx, trapz(ty, omega.*squeeze(BF(k,:,:)).*tmp));
+   end % for l
 end % for k
 
 % figure(2),
@@ -138,33 +146,33 @@ C = cgs(A,B');
 
 %% Reconstruction of numerical solution
 for k = ind
-    u_appr = u_appr + C(k)*squeeze(BF(k,:,:)).*omega;
+    u_appr = u_appr + C(k)*squeeze(BF(k,:,:)).*omega.*omega./dx;
 end % for k
 
 %% Plot the numerical solution
 figure(3),
 surf(x, y, u_appr)
-shading interp; lighting phong; camlight left;
-title('Numerical solution $u_{appr}(x,y)$','Interpreter','latex')
+shading interp;  
+title('Numerical solution u_{appr}(x,y)','Interpreter','tex')
 axis square
-xlabel('$x$','Interpreter','latex')
-ylabel('$y$','Interpreter','latex')
-zlabel('$u(x,y)$','Interpreter','latex')
+xlabel('x','Interpreter','tex')
+ylabel('y','Interpreter','tex')
+zlabel('u(x,y)','Interpreter','tex')
 
 figure(4)
 surf(x, y, ua(x,y))
-shading interp; lighting phong; camlight left;
-title('Exact solution $u(x,y)$','Interpreter','latex')
+shading interp;  
+title('Exact solution u(x,y)','Interpreter','tex')
 axis square
-xlabel('$x$','Interpreter','latex')
-ylabel('$y$','Interpreter','latex')
-zlabel('$u(x,y)$','Interpreter','latex')
+xlabel('x','Interpreter','tex')
+ylabel('y','Interpreter','tex')
+zlabel('u(x,y)','Interpreter','tex')
 
 %% Errors
 lhs = del2(u_appr, dx, dy) - rhs_func(x,y);
 
 figure(5),
 surf(x, y, lhs)
-shading interp; lighting phong; camlight left;
-title('Residual $res(x,y)=\triangle u_{appr}(x,y)+1$','Interpreter','latex')
+shading interp;  
+title('Residual res(x,y)=\Delta u_{appr}(x,y)+1','Interpreter','tex')
 axis square
