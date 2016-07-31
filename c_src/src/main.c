ee6b5031c7cf5fa344d07f41841633b4191635a2
VVBondarenko <vbondarenko3950@yandex.ru>
2016-07-31 20:20:37 +0300

    removed the most part of different branches, moved parallel_experimental to main branch

--- c_src/src/main.c
+++ c_src/src/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "B-splines.c"
 #include <gsl/gsl_linalg.h>
-#include "af_fourier.c"
+#include "af_poly.c"
 #include <string.h>
 #include <pthread.h>
 #include "smplDynArray.c"
@@ -26,34 +26,35 @@ double omega(double x, double y)
 double basis(double x, double y, int n)
 // Returns value of n-th \psi-basis function, used further, at point (x,y)
 {
-    return phi(x,y,n)*omega(x,y);
+		return phi(x,y,n)*omega(x,y);
 }
+
 #include "plotters.c"
-#include "simpson_integrals.c"
+#include "gauss_integrals.c"
 #include "error_functions.c"
-//testing variant
+
+
 double left_under_int(double x, double y, int m, int n)
 {
-
-    double  omega_px = omega(x + diff_step, y),
+ // \phi_m \Delta \phi_n 
+    double  omega_0  = omega(x,y),
+			omega_px = omega(x + diff_step, y),
             omega_mx = omega(x - diff_step, y),
             omega_py = omega(x, y + diff_step),
             omega_my = omega(x, y - diff_step),
-            phim_px  = phi(x+diff_step,y,m),
-            phim_py  = phi(x,y+diff_step,m),
-            phim_mx  = phi(x-diff_step,y,m),
-            phim_my  = phi(x,y-diff_step,m),
             phin_px  = phi(x+diff_step,y,n),
             phin_py  = phi(x,y+diff_step,n),
             phin_mx  = phi(x-diff_step,y,n),
             phin_my  = phi(x,y-diff_step,n);
 
-    return 0.25*glob_delta*glob_delta*
-           (     (omega_px*phin_px - omega_mx*phin_mx)*
-                 (omega_px*phim_px - omega_mx*phim_mx)+
-                 (omega_py*phin_py - omega_my*phin_my)*
-                 (omega_py*phim_py - omega_my*phim_my));
+    return omega_0*phi(x,y,m)*glob_delta*glob_delta*
+			(omega_px*phin_px + omega_mx*phin_mx + 
+			omega_py*phin_py + omega_my*phin_my - 4.*omega_0*phi(x,y,n));
+    
 }
+
+
+
 typedef struct rectangle_area {
     double x1,x2;
     double y1,y2;
@@ -88,7 +89,6 @@ void *parallel_pre_former2(void *arg)
         for(j = 0; j<N*N; j++)
             a->result[i+id*N*N/16][j] = integralLeft(left_under_int,a->area.x1,a->area.x2,a->area.y1,a->area.y2,i+id*N*N/16,j);
     }
-    //a->result = integralLeft(left_under_int,a->area.x1,a->area.x2,a->area.y1,a->area.y2,a->i,a->j);
     return 0;
 }
 
@@ -133,57 +133,10 @@ void form_matrix_parallel(gsl_matrix * system,
     return_code++;
     for(i = 0; i < N*N; i++)
     {
-        gsl_vector_set(RightPart, i, -integralRight(right_part_f,basis,x1,x2,y1,y2,i));
+		//for non variational solver remove minus
+        gsl_vector_set(RightPart, i, integralRight(right_part_f,basis,x1,x2,y1,y2,i));
         for(j = 0; j < N*N; j++)
             gsl_matrix_set(system, i,j, prematrixarray[i][j]);
-    }
-    free2DArr(&prematrixarray, N*N);
-}
-void form_matrix_parallel_v1_with_bug(gsl_matrix * system,
-                                      gsl_vector * RightPart,
-                                      double x1, double x2,
-                                      double y1, double y2)
-// Forms SLE system
-// system 	- left part matrix form of system
-// RightPart- right part vector of coefficients
-// x1, x2	- sizes of rectangle by x
-// y1, y2	- sizes of rectangle by y
-{
-    int i, j, return_code;
-    parallel_arg arg[N*N][N*N];
-    rectangle_area general_area;
-    general_area.x1 = x1;
-    general_area.x2 = x2;
-    general_area.y1 = y1;
-    general_area.y2 = y2;
-
-    double **prematrixarray;
-    init2DArr(&prematrixarray, N*N, N*N);
-    pthread_t threads[N*N][N*N];
-    for(i = 0; i < N*N; i++)
-        for(j = 0; j < N*N; j++)
-        {
-            arg[i][j].area = general_area;
-            arg[i][j].i = i;
-            arg[i][j].j = j;
-            return_code = pthread_create(&threads[i][j],NULL,parallel_pre_former, (void*)&arg[i][j]);
-        }
-
-    for(i = 0; i < N*N; i++)
-        for(j = 0; j < N*N; j++)
-        {
-            return_code = pthread_join(threads[i][j], NULL);
-            prematrixarray[i][j] = arg[i][j].result;
-        }
-    return_code++;
-    for(i = 0; i < N*N; i++)
-    {
-        gsl_vector_set(RightPart, i, -integralRight(right_part_f,basis,x1,x2,y1,y2,i));
-        for(j = 0; j < N*N; j++)
-        {
-            //gsl_matrix_set(system, i,j, integralLeft(left_under_int,x1,x2,y1,y2,i,j));
-            gsl_matrix_set(system, i,j, prematrixarray[i][j]);
-        }
     }
     free2DArr(&prematrixarray, N*N);
 }
@@ -243,7 +196,7 @@ int main(int argc, char **argv)
     //double a = A, b = B;
     N = atoi(argv[1]);
     intStep = (double) atoi(argv[2]);
-
+	initGaussInt();
     init_eq(atoi(argv[3]));
     init_basis(atoi(argv[4]));
     diff_step = pow(2.,-9);
@@ -264,8 +217,9 @@ int main(int argc, char **argv)
     solve_matrix_eq	(solution_glob, sys, rightpart);
     //solution_glob = solution;
     errors_to_stdio	(solution_glob, X0,X1, Y0,Y1);
-    //plot_region		(solution, X0,X1, Y0,Y1);
-    //plot_region_error	(solution, X0,X1, Y0,Y1);
+    multiplot		(solution_glob, X0,X1, Y0,Y1);
+    //plot_region		(solution_glob, X0,X1, Y0,Y1);
+    //plot_region_error	(solution_glob, X0,X1, Y0,Y1);
     //plot_exact_solution	(X0,X1, Y0,Y1);
     //plot_omega		(X0,X1, Y0,Y1);
 
--- c_src/src/plotters.c
+++ c_src/src/plotters.c
@@ -11,77 +11,114 @@ double reconstruct_at(gsl_vector *solution,
     return result;
 }
 
-
-void plot_region(gsl_vector *solution,
-                 double x1, double x2,
-                 double y1, double y2)
-// Plot solution in rectangle region 
-// from x1 till x2 by x, and from y1 till y2 by y
-
+void multiplot(gsl_vector *solution,
+               double x1, double x2,
+               double y1, double y2)
 {
     double hx = (x2-x1)/64.,
-    hy = (y2-y1)/64.,
-    i,j;
-    
+           hy = (y2-y1)/64.,
+           i,j;
+
     FILE * op;
     op = fopen("../plot_data/plot_region", "w");
     for(i=x1; i<=x2; i+=hx)
         for(j=y1; j<=y2; j+=hy)
             fprintf(op, "%f %f %f\n", i,j, reconstruct_at(solution,i,j));
     fclose(op);
-    i = system("../bin/Plot");
-}
 
-void plot_exact_solution(double x1, double x2,
-                         double y1, double y2)
-// Plot solution in rectangle region 
-// from x1 till x2 by x, and from y1 till y2 by y
-
-{
-    double	hx = (x2-x1)/64.,
-    hy = (y2-y1)/64.,
-    i,j;
-    
-    FILE * op;
     op = fopen("../plot_data/plot_exact_solution", "w");
-    
     for(i=x1; i<=x2; i+=hx)
         for(j=y1; j<=y2; j+=hy)
             fprintf(op, "%f %f %f\n", i,j, u_exact(i,j));
     fclose(op);
-    i = system("../bin/Plot_exact");
-}
 
-void plot_omega
-     (double x1, double x2, 
-      double y1, double y2)
-// Plot solution in rectangle region 
-// from x1 till x2 by x, and from y1 till y2 by y
-
-{
-    double	hx = (x2-x1)/64.,
-    hy = (y2-y1)/64.,
-    i,j;
-    
-    FILE * op;
     op = fopen("../plot_data/plot_plot_omega", "w");
-    
+
     for(i=x1; i<=x2; i+=hx)
         for(j=y1; j<=y2; j+=hy)
             fprintf(op, "%f %f %f\n", i,j, omega(i,j));
     fclose(op);
-    i = system("../bin/Plot_omega");
+
+    op = fopen("../plot_data/plot_region_error", "w");
+    for(i=x1; i<=x2; i+=hx)
+        for(j=y1; j<=y2; j+=hy)
+            fprintf(op, "%f %f %f\n", i,j, fabs(reconstruct_at(solution,i,j)-u_exact(i,j)));
+    fclose(op);
+
+    i = system("../bin/plotter_experimental.py &");
+}
+
+
+void plot_region(gsl_vector *solution,
+                 double x1, double x2,
+                 double y1, double y2)
+// Plot solution in rectangle region
+// from x1 till x2 by x, and from y1 till y2 by y
+
+{
+    double hx = (x2-x1)/64.,
+           hy = (y2-y1)/64.,
+           i,j;
+
+    FILE * op;
+    op = fopen("../plot_data/plot_region", "w");
+    for(i=x1; i<=x2; i+=hx)
+        for(j=y1; j<=y2; j+=hy)
+            fprintf(op, "%f %f %f\n", i,j, reconstruct_at(solution,i,j));
+    fclose(op);
+    i = system("../bin/plotter.py ../plot_data/plot_region Numerical &");
+}
+
+void plot_exact_solution(double x1, double x2,
+                         double y1, double y2)
+// Plot solution in rectangle region
+// from x1 till x2 by x, and from y1 till y2 by y
+
+{
+    double	hx = (x2-x1)/64.,
+            hy = (y2-y1)/64.,
+            i,j;
+
+    FILE * op;
+    op = fopen("../plot_data/plot_exact_solution", "w");
+
+    for(i=x1; i<=x2; i+=hx)
+        for(j=y1; j<=y2; j+=hy)
+            fprintf(op, "%f %f %f\n", i,j, u_exact(i,j));
+    fclose(op);
+    i = system("../bin/plotter.py ../plot_data/plot_exact_solution Exact &");
+}
+
+void plot_omega
+(double x1, double x2,
+ double y1, double y2)
+// Plot solution in rectangle region
+// from x1 till x2 by x, and from y1 till y2 by y
+
+{
+    double	hx = (x2-x1)/64.,
+            hy = (y2-y1)/64.,
+            i,j;
+
+    FILE * op;
+    op = fopen("../plot_data/plot_plot_omega", "w");
+
+    for(i=x1; i<=x2; i+=hx)
+        for(j=y1; j<=y2; j+=hy)
+            fprintf(op, "%f %f %f\n", i,j, omega(i,j));
+    fclose(op);
+    i = system("../bin/plotter.py ../plot_data/plot_plot_omega Omega &");
 }
 
 void plot_region_error(gsl_vector *solution,
                        double x1, double x2,
                        double y1, double y2)
-// Plot abs error of solution in rectangle region 
+// Plot abs error of solution in rectangle region
 // from x1 till x2 by x, and from y1 till y2 by y
 {
     double hx = (x2-x1)/64.,
-    hy = (y2-y1)/64.,
-    i,j;
+           hy = (y2-y1)/64.,
+           i,j;
 
     FILE * op;
     op = fopen("../plot_data/plot_region_error", "w");
@@ -89,5 +126,5 @@ void plot_region_error(gsl_vector *solution,
         for(j=y1; j<=y2; j+=hy)
             fprintf(op, "%f %f %f\n", i,j, fabs(reconstruct_at(solution,i,j)-u_exact(i,j)));
     fclose(op);
-    i = system("../bin/Plot_err");
+    i = system("../bin/plotter.py ../plot_data/plot_region_error Error &");
 }
