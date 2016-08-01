cc2bac27273f051f02a8197139166d70baa86681
VVBondarenko <vbondarenko3950@yandex.ru>
2016-08-01 22:28:29 +0300

    moved parallel former to separate file, 'cause it's not required now

--- c_src/src/main.c
+++ c_src/src/main.c
@@ -30,8 +30,8 @@ double basis(double x, double y, int n)
 }
 
 #include "plotters.c"
-#include "gauss_integrals.c"
-#include "error_functions.c"
+#include "gauss_integrals.c" //todo: reduce file to single function
+#include "error_functions.c" 
 
 
 double left_under_int(double x, double y, int m, int n)
@@ -53,93 +53,11 @@ double left_under_int(double x, double y, int m, int n)
     
 }
 
-
-
-typedef struct rectangle_area {
-    double x1,x2;
-    double y1,y2;
-} rectangle_area;
-
-typedef struct parallel_arg {
-    rectangle_area area;
-    int i, j;
-    double result;
-} parallel_arg;
-
-typedef struct parallel_arg2 {
-    rectangle_area area;
-    int threadId;
-    double **result;
-} parallel_arg2;
-
-void *parallel_pre_former(void *arg)
-{
-    parallel_arg *a = (parallel_arg *)arg;
-    a->result = integralLeft(left_under_int,a->area.x1,a->area.x2,a->area.y1,a->area.y2,a->i,a->j);
-    return 0;
-}
-
-void *parallel_pre_former2(void *arg)
-{
-    parallel_arg2 *a = (parallel_arg2 *)arg;
-    int id = a->threadId;
-    int i, j;
-    for(i = 0; i<N*N/16; i++)
-    {
-        for(j = 0; j<N*N; j++)
-            a->result[i+id*N*N/16][j] = integralLeft(left_under_int,a->area.x1,a->area.x2,a->area.y1,a->area.y2,i+id*N*N/16,j);
-    }
-    return 0;
-}
-
-void form_matrix_parallel(gsl_matrix * system,
-                          gsl_vector * RightPart,
-                          double x1, double x2,
-                          double y1, double y2)
-// Forms SLE system
-// system 	- left part matrix form of system
-// RightPart- right part vector of coefficients
-// x1, x2	- sizes of rectangle by x
-// y1, y2	- sizes of rectangle by y
-{
-    int i, j, k, return_code;
-    parallel_arg2 arg[16];
-    rectangle_area general_area;
-    general_area.x1 = x1;
-    general_area.x2 = x2;
-    general_area.y1 = y1;
-    general_area.y2 = y2;
-
-    double **prematrixarray;
-    init2DArr(&prematrixarray, N*N, N*N);
-    pthread_t threads[16];
-
-    for(i = 0; i < 16; i++)
-    {
-        arg[i].area = general_area;
-        arg[i].threadId = i;
-        arg[i].result = prematrixarray;
-        return_code=pthread_create(&threads[i],NULL,parallel_pre_former2, (void*)&arg[i]);
-    }
-
-    for(k = 0; k < 16; k++)
-    {
-        return_code = pthread_join(threads[k], NULL);
-
-        for(i = 0; i<N*N/16; i++)
-            for(j = 0; j < N*N; j++)
-                prematrixarray[i+k*N*N/16][j]=arg[i].result[i+k*N*N/16][j];
-    }
-    return_code++;
-    for(i = 0; i < N*N; i++)
-    {
-		//for non variational solver remove minus
-        gsl_vector_set(RightPart, i, integralRight(right_part_f,basis,x1,x2,y1,y2,i));
-        for(j = 0; j < N*N; j++)
-            gsl_matrix_set(system, i,j, prematrixarray[i][j]);
-    }
-    free2DArr(&prematrixarray, N*N);
-}
+/*
+ * 
+ * Place for Including of parallel former, if required
+ * 
+ */
 
 void form_matrix (gsl_matrix * system,
                   gsl_vector * RightPart,
@@ -168,7 +86,6 @@ void solve_matrix_eq(gsl_vector * solution,
 //Solve SLE Ax=b, where A = system, b = RightPart, x = solution
 {
     int i;
-
     gsl_permutation * p = gsl_permutation_alloc (N*N);
     gsl_linalg_LU_decomp (system, p, &i);
     gsl_linalg_LU_solve (system, p, RightPart, solution);
@@ -208,11 +125,6 @@ int main(int argc, char **argv)
     solution_glob = gsl_vector_alloc(N*N);
     form_matrix		(sys, rightpart, X0,X1, Y0,Y1);
     //form_matrix_parallel(sys, rightpart, X0,X1, Y0,Y1);
-
-    //FILE *op;
-    //op = fopen("./matrix", "w");
-    //gsl_matrix_fprintf(op, sys, "%f");
-    //fclose(op);
 
     solve_matrix_eq	(solution_glob, sys, rightpart);
     //solution_glob = solution;
