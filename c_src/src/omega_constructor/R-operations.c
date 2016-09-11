19a91eb62ecb420a87ab629195eb2199f189b0f0
VVBondarenko <vbondarenko3950@yandex.ru>
2016-09-11 23:14:43 +0300

    fixed bugs in primitives added test plotter

--- c_src/src/omega_constructor/R-operations.c
+++ c_src/src/omega_constructor/R-operations.c
@@ -13,3 +13,36 @@ double R_or(double x, double y)
 }
 
 
+void plot_func(double (*f)(double, double))
+// Plot solution in rectangle region
+// from x1 till x2 by x, and from y1 till y2 by y
+
+{
+	double x0 = -1., x1 = 1., y0 = -1., y1 = 1.;
+    double hx = (x1-x0)/128.,
+           hy = (y1-y0)/128.,
+           i,j;
+
+    FILE * op;
+    op = fopen("../../plot_data/plot_region", "w");
+    for(i=x0; i<=x1; i+=hx)
+        for(j=y0; j<=y1; j+=hy)
+        {
+            fprintf(op, "%15.15f %15.15f %15.15f\n", i,j, (*f)(i,j));
+        }
+    fclose(op);
+    i = system("../../bin/plotter.py ../../plot_data/plot_region Numerical &");
+}
+
+int main()
+{
+	omega_primitive A = {.x0 = 0., .y0 = 0., .a = 2., .b = 2./5};
+	double figure1(double x, double y)
+	{
+		return fmax(parabola(A, x,y),0);
+	}
+	
+	plot_func(figure1);
+	
+	return 0;
+}
--- c_src/src/omega_constructor/main_omega.c
+++ c_src/src/omega_constructor/main_omega.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <math.h>
 
-typedef struct omega_primitive{
-	double x0, y0;
-	double a, b;
-}omega_primitive;
+typedef struct omega_primitive {
+    double x0, y0;
+    double a, b;
+} omega_primitive;
 
 /*
  * полоса || Ox		band_y			+
@@ -15,102 +15,86 @@ typedef struct omega_primitive{
  * эллипс			ellipse			+
  * треугольник		triangle		-
  * полуплоскость	half_plane		+
- * 
- * 
+ *
+ *
  * парабола			parabola		+
  * угол				angle			-
- * 
+ *
  */
- 
- double band_y(omega_primitive properties, double x, double y){
-	double X = x - properties.x0;
-	double width = properties.a;
-	
-	return 0.5/width*(width-X)*(width-X);
+
+double band_y(omega_primitive properties, double x, double y) {
+    double X = x - properties.x0;
+    double width = properties.a;
+
+    return 0.5/width*(width-X)*(width+X);
 }
 
-double band_x(omega_primitive properties, double x, double y){
-	double Y = y - properties.y0;
-	double height = properties.b;
-	
-	return 0.5/height*(height-Y)*(height-Y);
+double band_x(omega_primitive properties, double x, double y) {
+    double Y = y - properties.y0;
+    double height = properties.b;
+
+    return 0.5/height*(height-Y)*(height+Y);
 }
 
-double rectangle(omega_primitive properties, double x, double y){
-	double X = x - properties.y0;
-	double Y = y - properties.y0;
-	double width = properties.a;
-	double width = properties.b;
-	
-	double val01 = 0.5/width*(width-X)*(width-X);
-	double val02 = 0.5/height*(height-Y)*(height-Y));
-	
-	
-	return val01 + val02 - sqrt(val01*val01 + val02*val02);
+double rectangle(omega_primitive properties, double x, double y) {
+    double X = x - properties.x0;
+    double Y = y - properties.y0;
+    double width = properties.a;
+    double height = properties.b;
+
+    double val01 = 0.5/width*(width-X)*(width+X);
+    double val02 = 0.5/height*(height-Y)*(height+Y);
+
+
+    return val01 + val02 - sqrt(val01*val01 + val02*val02);
 }
 
-double circle(omega_primitive properties, double x, double y){
-	double X = x - properties.y0;
-	double Y = y - properties.y0;
-	double radius = properties.a;
-			
-	return 0.5/radius*(radius*radius-X*X-Y*Y);
+double circle(omega_primitive properties, double x, double y) {
+    double X = x - properties.x0;
+    double Y = y - properties.y0;
+    double radius = properties.a;
+
+    return 0.5/radius*(radius*radius-X*X-Y*Y);
 }
 
-double ellipse(omega_primitive properties, double x, double y){
-	double X = x - properties.y0;
-	double Y = y - properties.y0;
-	double a = properties.a;
-	double b = properties.b;
-	
-	double a2 = a*a;
-	double b2 = b*b;
-	double X2 = X*X;
-	double Y2 = Y*Y;
-	
-	double val01 = a2*b2 - b2*X2 - a2*Y2;
-	double val02 = 0.5/(a*b)*sqrt(a2 + b2 - X2 - Y2);
-			
-	return val01/val02;
+double ellipse(omega_primitive properties, double x, double y) {
+    double X = x - properties.x0;
+    double Y = y - properties.y0;
+    double a = properties.a;
+    double b = properties.b;
+
+    double a2 = a*a;
+    double b2 = b*b;
+    double X2 = X*X;
+    double Y2 = Y*Y;
+
+    double val01 = a2*b2 - b2*X2 - a2*Y2;
+    double val02 = 0.5/(a*b)*sqrt(a2 + b2 - X2 - Y2);
+
+    return val01/val02;
 }
 
-double ellipse(omega_primitive properties, double x, double y){
-	double X = x - properties.y0;
-	double Y = y - properties.y0;
-	double a = properties.a;
-	double b = properties.b;
-	
-	double a2 = a*a;
-	double b2 = b*b;
-	double X2 = X*X;
-	double Y2 = Y*Y;
-	
-	double val01 = a2*b2 - b2*X2 - a2*Y2;
-	double val02 = 0.5/(a*b)*sqrt(a2 + b2 - X2 - Y2);
-			
-	return val01/val02;
+
+double half_plane(omega_primitive properties, double x, double y) {
+    double X = x - properties.x0;
+    double Y = y - properties.y0;
+    double a = properties.a;
+    double b = properties.b;
+
+    double a2 = a*a;
+    double b2 = b*b;
+
+    return (a*X+b*Y)/sqrt(a2+b2);
 }
 
-double half_plane(omega_primitive properties, double x, double y){
-	double X = x - properties.y0;
-	double Y = y - properties.y0;
-	double a = properties.a;
-	double b = properties.b;
-			
-	double a2 = a*a;
-	double b2 = b*b;
-			
-	return (a*X+b*Y)/sqrt(a2+b2);
-}
+double parabola(omega_primitive properties, double x, double y) {
+    double X = x - properties.x0;
+    double Y = y - properties.y0;
+    double a = properties.a;
+    double b = properties.b;
 
-double parabola(omega_primitive properties, double x, double y){
-	double X = x - properties.y0;
-	double Y = y - properties.y0;
-	double a = properties.a;
-	double b = properties.b;
-	
-	double a2 = a*a;
-	double X2 = X*X;
-			
-	return (b-a*X2-Y)/sqrt(1+a2*X2);
+    double a2 = a*a;
+    double X2 = X*X;
+
+    return (b-a*X2-Y)/sqrt(1+a2*X2);
 }
