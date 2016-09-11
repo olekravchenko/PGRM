d636afcf1546abeb87c08ee1579a6afdda1a6116
VVBondarenko <vbondarenko3950@yandex.ru>
2016-09-11 21:34:18 +0300

    added description for further omega primitives

---
+++ c_src/src/omega_constructor/main_omega.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct omega_primitive{
+	double x0, y0;
+	double a, b;
+	
+}omega_primitive;
+
+/*
+ * полоса
+ * прямоугольник
+ * круг (эллипс)
+ * треугольник
+ * полуплоскость
+ * 
+ * 
+ * парабола
+ * угол
+ * 
+ */
